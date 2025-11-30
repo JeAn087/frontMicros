b7d26c2cb59cabd60a54156ea4e12902eef705ca
Jeison Andrés Vallejo Gómez <jeisonvallejo@unicauca.edu.co>
2025-11-30 07:23:27 -0500

    Revert "Skelethon Conection"
    This reverts commit a40cdb57e6e9b5419d3d532a8133ef19b19f10f5.

--- src/services/userService.ts
+++
@@ -1,8 +0,0 @@
-import { api } from '../api/client';
-import type { UsuarioDTO } from '../types/users/response';
-
-export const usuarioService = {
-    getAll: () => api.get<UsuarioDTO[]>('/usuarios'),
-
-    getById: (id: number) => api.get<UsuarioDTO>(`/usuarios/${id}`),
-};
